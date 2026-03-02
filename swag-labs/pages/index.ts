955b49dcb6bb40a6d305caf4693d0039af98ed2f
UhgoFR <uhgo.flores@gmail.com>
2026-03-02 14:19:50 -0600

    Refactor page structure and update page implementations

--- swag-labs/pages/index.ts
+++ swag-labs/pages/index.ts
@@ -1,4 +1,4 @@
-export { BasePage } from './BasePage';
+export { BasePage } from '../../pages/BasePage';
 export { LoginPage } from './LoginPage';
 export { ProductsPage } from './ProductsPage';
 export { CheckoutPage } from './CheckoutPage';
